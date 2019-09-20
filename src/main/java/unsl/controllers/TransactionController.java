d7fa892a13c7686e02f13114cf942cd5d66f6c26
Soathekid <sebaoaki@gmail.com>
2019-09-20 17:26:29 -0300

    controller modificado

---
+++ src/main/java/unsl/controllers/TransactionController.java
@@ -0,0 +1,13 @@
+package unsl.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import unsl.entities.ResponseError;
+import unsl.entities.Transaction;;
+import unsl.services.TransactionServices;
+
+
--- src/main/java/unsl/entities/Transaction.java
+++ src/main/java/unsl/entities/Transaction.java
@@ -1,35 +1,80 @@
 package unsl.entities;
+
 import javax.persistence.*;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
 @Entity
 
-
-
 public class Transaction {
-    
-    public static enum Status{
-        PENDIENTE,
-        PROCESADA,
-        CANCELADA
+
+    public static enum Status {
+        PENDIENTE, PROCESADA, CANCELADA
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @JsonProperty("")
+
+    @JsonProperty("origin_account_id")
     private long origin_account_id;
 
-    @JsonProperty("")
+    @JsonProperty("destination_account_id")
     private long destination_account_id;
 
-    @JsonProperty("")
+    @JsonProperty("amount")
     private float amount;
-    
+
     private String date;
 
     @Enumerated(EnumType.STRING)
     private Status status;
 
+    
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public long getDestination_account_id() {
+        return destination_account_id;
+    }
+
+    public void setDestination_account_id(long destination_account_id) {
+        this.destination_account_id = destination_account_id;
+    }
+
+    public long getOrigin_account_id() {
+        return origin_account_id;
+    }
+
+    public void setOrigin_account_id(long origin_account_id) {
+        this.origin_account_id = origin_account_id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+   
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
