a89e3550a87b5d12523f50cceb07cfdd72143352
Soathekid <sebaoaki@gmail.com>
2019-09-20 23:29:42 -0300

    transaction controller

--- src/main/java/unsl/controllers/TransactionController.java
+++ src/main/java/unsl/controllers/TransactionController.java
@@ -1,8 +1,17 @@
 package unsl.controllers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,32 +20,66 @@ import unsl.entities.Transaction;
 import unsl.services.TransactionServices;
 
 @RestController
-public class TransactionController{
-    
+//@RequestMapping("/transacciones")
+public class TransactionController {
+
     @Autowired
     TransactionServices transactionService;
-    
-    @GetMapping(value ="/transactions")
+
+    @GetMapping(value = "/transactions")
     @ResponseBody
-    @ResponseStatus(HttpStatus.OK)  
-    public List<Transaction> getAll(){
+    @ResponseStatus(HttpStatus.OK)
+    public List<Transaction> getAll() {
         return transactionService.getAll();
     }
-     
-    @GetMapping(value="/transactions/{id}")
+
+    @GetMapping(value = "/transactions/{id}")
     @ResponseBody
-    public Object getTransaction(@PathVariable("id")long transactionId){
-           Transaction transaction = transactionService.getTransaction(transactionId);
-           if(transaction==null){
-               return new ResponseEntity(new ResponseError(404,String.format("Transaction %d not found", transaction.getId())),HttpStatus.NOT_FOUND);
-           }
-           return transaction;
+    public Object getTransaction(@PathVariable("id") long transactionId) {
+        Transaction transaction = transactionService.getTransaction(transactionId);
+        if (transaction == null) {
+            return new ResponseEntity(
+                    new ResponseError(404, String.format("Transaction %d not found", transaction.getId())),
+                    HttpStatus.NOT_FOUND);
+        }
+        return transaction;
     }
-    
+
     @PostMapping(value = "/transactions")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public Object createTransaction(@RequestBody Transaction transaction){
+    public Object createTransaction(@RequestBody Transaction transaction) {
+
+        // supposed this is your FirstController url.
+        String url = "http://localhost:8889/accounts/"+transaction.getOrigin_account_id();
+        // create request.
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+        // execute your request.
+        
+        try {
+            HttpResponse response = client.execute(request);
+            
+            BufferedReader rd = new BufferedReader(
+		    new InputStreamReader(response.getEntity().getContent()));
+             
+          //  BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            while ((line = rd.readLine()) != null) // Read line by line
+                sb.append(line + "\n");
+
+            String resString = sb.toString(); // Result is here
+            
+            System.out.println("esto tiro la request "+resString);
+
+            rd.close();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
        
         return transactionService.saveTransaction(transaction);
     
