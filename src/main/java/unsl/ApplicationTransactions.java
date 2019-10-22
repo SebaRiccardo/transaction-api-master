1b77a0b2e618f5de109d38a9d2f0321b10da2c9d
Soathekid <sebaoaki@gmail.com>
2019-10-22 01:14:41 -0300

    comentarios

--- src/main/java/unsl/ApplicationTransactions.java
+++ src/main/java/unsl/ApplicationTransactions.java
@@ -4,19 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
+//@EnableEurekaClient
 public class ApplicationTransactions extends SpringBootServletInitializer{
 	
 
 	@Bean
-	@Primary
-	@LoadBalanced
+	//@LoadBalanced
 	public RestTemplate  getRestTemplate2() {
  	return new RestTemplate();
 	}
